0f0092c918c279689e29b7fc320611fe814b4601
imagemlt <malingtao1019@163.com>
2026-01-22 16:14:11 +0800

    Add startup script and SYSTEMD

---
+++ amldigitalfpv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+if [ -f /storage/digitalfpv/wfb.conf ]; then
+	. /storage/digitalfpv/wfb.conf
+else
+	. /flash/wfb.conf
+fi
+
+if [ -f /storage/streamer/AMLDigitalFPV.new ]; then
+	mv /storage/streamer/AMLDigitalFPV.new /storage/streamer/AMLDigitalFPV
+fi
+
+export GST_PLUGIN_PATH=/storage/streamer/gstreamer/
+export GST_PLUGIN_SCANNER=/storage/streamer/gstreamer/gst-plugin-scanner
+export LD_LIBRARY_PATH=$GST_PLUGIN_PATH
+
+if [ ! -f /tmp/added_vfm ] ; then
+	echo '[+]add VFM map'
+	echo 'add vdec-map-0 vdec.h265.00 amvideo' >/sys/class/vfm/map
+	touch /tmp/added_vfm
+fi
+
+if [ "$bad_frame" != "0" ]; then
+	echo "[+]Error policy: Ignore ERROR"
+	echo 1 > /sys/module/amvdec_h265/parameters/hacked_lowlatency
+	echo 0 >/sys/module/amvdec_h265/parameters/nal_skip_policy
+	echo 0 >/sys/module/amvdec_h265/parameters/ref_frame_mark_flag
+	echo ${bad_frame} > /sys/module/amvdec_h265/parameters/error_handle_policy
+else
+	echo "[+]Error policy: Drop FRAME"
+fi
+
+/storage/streamer/AMLDigitalFPV -w ${video_width} -h ${video_height} -p 120 -a ${sound} -l ${buf_level}
